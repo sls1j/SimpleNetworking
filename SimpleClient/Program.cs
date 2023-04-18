98c33393db4cceb709ea06b9b54712b04cdadcfa
Brian Dickey <brian@bee-eee.com>
2023-04-18 11:13:34 -0600

    Add project files.

---
+++ SimpleClient/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+
+Client client = new Client("localhost", 45001);
+
+// send 10 messages
+for (int i = 0; i < 10; i++)
+{
+  // send a message
+  client.WriteMessage($"MESSAGE {i}");
+
+  // wait for the response
+  string response = client.ReadMessage();
+  Console.WriteLine($"Received response: {response}");
+}
+
+client.Close();
+
+// Defines the Client logic
+class Client
+{
+  TcpClient _client;
+  NetworkStream _stream;
+
+  public Client(string host, int port)
+  {
+    _client = new TcpClient(host, port);
+    _stream = _client.GetStream();
+  }
+
+  /// <summary>
+  /// Reads data from the socket until a 0 character is received or a -1 (end of stream) is received
+  /// </summary>
+  /// <returns></returns>
+  /// <exception cref="InvalidOperationException"></exception>
+  public string ReadMessage()
+  {
+    // read into a buffer
+    int count = 0;
+    StringBuilder message = new StringBuilder();
+    while (true)
+    {
+      int b = _stream.ReadByte();
+      if (b == 0)
+        break;
+      else
+      {
+        message.Append((char)b);
+        count++;
+      }
+
+      if (count > 4096)
+        throw new InvalidOperationException("Message too big! Aborting connection");
+    }
+
+    return message.ToString();
+  }
+
+  /// <summary>
+  /// Writes data to the socket then a 0 to indicate that it's finished
+  /// </summary>
+  /// <param name="message"></param>
+  public void WriteMessage(string message)
+  {
+    byte[] buffer = Encoding.UTF8.GetBytes(message);
+    _stream.Write(buffer, 0, buffer.Length);
+    _stream.WriteByte(0); // this tells the stream on the other side the message is done.
+  }
+
+  /// <summary>
+  /// Closes the socket
+  /// </summary>
+  public void Close()
+  {
+    _client.Close();   
+  }
+}
+
+// You can define other methods, fields, classes and namespaces here
---
+++ SimpleServer/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+List<Receiver> receivers = new List<Receiver>();
+TcpListener listener = new TcpListener(IPAddress.Any, 45001);
+listener.Start();
+while (true)
+{
+  Console.WriteLine("Waiting for client to connect...");
+  TcpClient client = listener.AcceptTcpClient();
+  Console.WriteLine($"New client connected via {client.Client.RemoteEndPoint.ToString()}");
+  receivers.Add(new Receiver(client));
+}
+
+// You can define other methods, fields, classes and namespaces here
+
+class Receiver
+{
+  private TcpClient _client;
+  private NetworkStream _stream;
+  public Receiver(TcpClient client)
+  {
+    _client = client;
+    _stream = _client.GetStream();
+
+    // starts the loop to read messages from the client
+    ThreadPool.QueueUserWorkItem(MessageLoop);
+  }
+
+  private void MessageLoop(object o)
+  {
+    Console.WriteLine($"Is Connected? {_client.Connected.ToString()}");
+    using NetworkStream stream = _client.GetStream();
+    while (_client.Connected)
+    {
+      string message = ReadMessage();
+      if (!string.IsNullOrEmpty(message))
+      {
+        // write out response
+        string outMessage = $"ACK {message}";
+        WriteMessage(outMessage);
+      }
+      else
+        break;
+    }
+
+    Console.WriteLine("Client disconnected");
+  }
+
+  string ReadMessage()
+  {
+    // read into a buffer
+    int count = 0;
+    StringBuilder message = new StringBuilder();
+    try
+    {
+      while (true)
+      {
+        int b = _stream.ReadByte();
+        if (b <= 0)
+          break;
+        else
+        {
+          message.Append((char)b);
+          count++;
+        }
+
+        if (count > 4096)
+          throw new InvalidOperationException("Message too big! Aborting connection");
+      }
+    }
+    catch (Exception)
+    {
+      // if something goes wrong we'll just swallow the error
+      Console.WriteLine("Failed to read.");
+    }
+    
+    string m = message.ToString();
+    if (!string.IsNullOrEmpty(m))
+    {
+      Console.WriteLine($"Received: {m}");
+    }
+
+    return m;
+  }
+
+  void WriteMessage(string message)
+  {
+    try
+    {
+      byte[] buffer = Encoding.UTF8.GetBytes(message);
+      _stream.Write(buffer, 0, buffer.Length);
+      _stream.WriteByte(0); // this tells the stream on the other side the message is done.
+
+      Console.WriteLine($"Send: {message}");
+    }
+    catch (Exception)
+    {
+      Console.WriteLine("Failed to send message");
+    }
+  }
+}
